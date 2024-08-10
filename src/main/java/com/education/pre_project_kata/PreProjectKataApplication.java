3e8b00844e7a83c547b5325f027a6559592f0a78
Derian50 <Den4ik719.cr.2@gmail.com>
2024-08-10 13:08:53 +0300

    init

---
+++ src/main/java/com/education/pre_project_kata/PreProjectKataApplication.java
@@ -0,0 +1,13 @@
+package com.education.pre_project_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreProjectKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/education/pre_project_kata/PreProjectKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.pre_project_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
